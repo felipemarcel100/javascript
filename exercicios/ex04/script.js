d1ac61a2375327a2bb4c940d6ad200875795ee95
felipemarcel100 <felipemarcel100@hotmail.com>
2023-02-16 09:29:53 -0300

    update

--- exercicios/ex04/script.js
+++ exercicios/ex04/script.js
@@ -1,14 +1,20 @@
 
 
 function calcular(){
-    var mult = Number(document.getElementById('mult').value)
-    var res = document.getElementById('res')
-    var cont = 0
-    //window.alert(`${mult}`)
-    
-    while(cont <= 10){
-        res.innerHTML += `${mult} x ${cont} = ${mult*cont} <br>`
-        cont++
+    let num = Number(document.getElementById('mult').value)
+    let tab = document.getElementById('seltab')
+
+    if(num.length == 0){
+        window.alert('Digite um número válido')
+    } else{
+        let cont = 0
+        tab.innerHTML = ''
+        while(cont <= 10){
+            let item = document.createElement('option')
+            item.text = `${num} x ${cont} = ${num*cont}`
+            //item.value = `tab${cont}`
+            tab.appendChild(item)
+            cont++
+        }
     }
-    
 }
